0d8a7ad76bf9d723e2f62bd529b97a470863f36c
Arthur Nardini <arthur.nardini03@gmail.com>
2025-03-27 16:29:16 -0300

    feat(): Adiciona componentes necessários para a criação da tela

---
+++ src/presentation/components/ui/paper.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib";
+import { cva, VariantProps } from "class-variance-authority";
+
+const paperVariants = cva(
+  "text-mtext border-2 border-border shadow-shadow inline-flex items-center justify-center whitespace-nowrap rounded-base text-sm font-base ring-offset-white transition-all gap-2 [&_svg]:pointer-events-none [&_svg]:shrink-0 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none",
+  {
+    variants: {
+      color: {
+        default: "bg-bg",
+        primary: "bg-main",
+        blue: "bg-main-blue",
+        orange: "bg-main-orange",
+        pink: "bg-main-pink",
+        green: "bg-main-green",
+        purple: "bg-main-purple",
+      },
+    },
+    defaultVariants: {
+      color: "default",
+    },
+  }
+);
+
+interface Props extends VariantProps<typeof paperVariants> {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+}
+
+export function Paper({ children, className, onClick }: Props) {
+  return (
+    <div onClick={onClick} className={cn(paperVariants({ className }))}>
+      {children}
+    </div>
+  );
+}
