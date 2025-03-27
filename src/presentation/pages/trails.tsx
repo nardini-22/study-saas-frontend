f3c9306db99ed86fdd88857d2a5e78917e9e182b
Arthur Nardini <arthur.nardini03@gmail.com>
2025-03-27 17:27:24 -0300

    feat(trails): Adiciona listagem das trilhas

--- src/presentation/pages/trails.tsx
+++ src/presentation/pages/trails.tsx
@@ -1,12 +1,41 @@
 "use client";
 
-import { ITrailsContract } from "@/domain/models/trails";
-import { ModalCreateTrail } from "../components";
+import { ITrails, ITrailsContract } from "@/domain/models/trails";
+import { ModalCreateTrail, Paper } from "../components";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 interface Props {
   service: ITrailsContract;
 }
 
 export function TrailsPage({ service }: Props) {
-  return <ModalCreateTrail service={service} />;
+  const [trails, setTrails] = useState<Array<ITrails>>([]);
+  const fetchTrails = async () => {
+    try {
+      const data = await service.getTrails();
+      setTrails(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrails();
+  }, []);
+
+  return (
+    <div className="flex gap-8">
+      {trails.length > 0 &&
+        trails.map((trail) => (
+          <Link key={trail.id} href={`/trails/${trail.id}`}>
+            <Paper className="flex-col p-4">
+              <h1 className="text-2xl">{trail.name}</h1>
+              <p>{trail.description}</p>
+            </Paper>
+          </Link>
+        ))}
+      <ModalCreateTrail service={service} />
+    </div>
+  );
 }
