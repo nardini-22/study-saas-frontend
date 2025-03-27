cc4527a45e1254037c9e889d65aa0ba2628ca643
Arthur Nardini <arthur.nardini03@gmail.com>
2025-03-27 17:10:42 -0300

    feat(trails): Adiciona trail service e corrige trails contract

--- src/domain/models/trails/trails.contract.ts
+++ src/domain/models/trails/trails.contract.ts
@@ -1,11 +1,16 @@
 import { ICreateTrail, ITrails } from "./trails.model";
 
 export interface ITrailsContract {
-  createTrail(props: ITrailsContract.createTrailProps): Promise<ITrails>;
+  createTrail(
+    props: ITrailsContract.createTrailProps
+  ): Promise<ITrailsContract.createTrailResponse>;
+  getTrails(): Promise<ITrailsContract.getTrailsResponse>;
 }
 
 export namespace ITrailsContract {
   export type createTrailProps = {
     body: ICreateTrail;
   };
+  export type createTrailResponse = ITrails;
+  export type getTrailsResponse = Array<ITrails>;
 }
