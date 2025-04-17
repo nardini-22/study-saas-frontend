503a0bf9ce560bfa22754cd114490b79243c80f8
Arthur Nardini <arthur.nardini03@gmail.com>
2025-04-17 18:28:48 -0300

    chore: Atualiza tailwind para v4

--- src/presentation/components/ui/dialog.tsx
+++ src/presentation/components/ui/dialog.tsx
@@ -50,7 +50,7 @@ const DialogContent = React.forwardRef<
       {children}
       {!hideCloseButton && (
         <DialogPrimitive.Close
-          className="absolute right-4 top-4 rounded-sm opacity-100 ring-offset-white focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-white data-[state=open]:text-white"
+          className="absolute right-4 top-4 rounded-xs opacity-100 ring-offset-white focus:outline-hidden focus:ring-2 focus:ring-black focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-white data-[state=open]:text-white"
           disabled={loading}
         >
           <X className="h-4 w-4" />
